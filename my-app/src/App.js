1a1fe2d4aad8d0f94be5ba2fb4b8fd2921327a36
Aman Agrawal <amanagrawal2098@gmail.com>
2020-01-27 23:52:17 +0530

    added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,30 @@
-import React,{Component, useState} from "react";
+import React,{Component, useState, useEffect} from "react";
 //import Student from "./student";
 function App()
 {
-    const nameStateVariable=useState("Aman");
+    const [count,setCount]=useState(0);
+    const [count1,setCount1]=useState(50);
     const handleclick=()=>{
-        nameStateVariable[1]("Geeky Shows");
+        setCount(count+1);
     };
+    const handleclick1=()=>{
+        setCount1(count1-1);
+    };
+    useEffect(
+        ()=>{
+            console.log("Use effect called");
+        },[count]//giving another param in order to make useeffect run fir just count1
+    );
     return(
         <>
-        <h1>{nameStateVariable[0]}</h1>
+        <h1>Count:{count}</h1>
         <button type="button" onClick={handleclick}>
-            Change
+            Increment
+        </button>
+        
+        <h1>Count:{count1}</h1>
+        <button type="button" onClick={handleclick1}>
+            Decrement
         </button>
         </>
     )
