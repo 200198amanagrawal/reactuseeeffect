66fbf1bdcdb15a950b89ee8294dddb43cc1575a2
Aman Agrawal <amanagrawal2098@gmail.com>
2020-01-27 22:43:14 +0530

    yup

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,46 +1,18 @@
-import React,{Component} from "react";
-import Student from "./student";
-/*
-class App extends Component
+import React,{Component, useState} from "react";
+//import Student from "./student";
+function App()
 {
-    render()
-    {
-        return (
-            <>
-                <Student roll="123"/>
-            </>
-        );
-        //could also have returned return <h1>hello world!!</h1>.
-    }
+    const nameStateVariable=useState("Aman");
+    const handleclick=()=>{
+        nameStateVariable[1]("Geeky Shows");
+    };
+    return(
+        <>
+        <h1>{nameStateVariable[0]}</h1>
+        <button type="button" onClick={handleclick}>
+            Change
+        </button>
+        </>
+    )
 }
-export default App
-/*const App=(props)=>{
-return <h1>Hello world {props.name}</h1>;
-};
-;*/
-
-export default class App extends Component
-{
-    constructor(props)
-    {
-        super(props);
-        console.log(props);
-        this.state={
-            roll:101
-        };
-    }
-    static getDerivedStateFromProps(props,state)
-    {
-        console.log("get derivied");
-        console.log(state);
-        return null;
-    }
-    render()
-    {
-        console.log("App remdered");
-        return (
-            <>
-        <Student name="Aman"></Student>
-        
-        </>)    }
-}
+export default App;
